44d0eba2cc052a1086a23caf5f23b32714050af8
QuentinGP23 <quentgeoffpro@yahoo.com>
2023-03-27 19:13:59 +0200

    .

---
+++ js/loader.js
@@ -0,0 +1,5 @@
+// Une fois que la page a fini de charger, le loader sera caché
+window.addEventListener("load", function() {
+    document.getElementById("loader-wrapper").style.display = "none";
+  });
+  
