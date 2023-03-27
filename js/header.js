443930775ef7be2776656783ed5cf5a3a75a3ea4
QuentinGP <quentgeoffpro@yahoo.com>
2023-03-27 09:51:21 +0200

    .

--- js/header.js
+++ js/header.js
@@ -1,15 +1,18 @@
 const nav = document.getElementById("nav");
 const toggleIcon = document.getElementById("toggleIcon");
 const main = document.getElementsByTagName("main")[0];
+const footer = document.getElementsByTagName("footer")[0];
 
 toggleIcon.addEventListener("click", function() {
   if (main.style.display === "none") {
     nav.style.display = "none";
     main.style.display = "block";
+    footer.style.display = "flex";
     toggleIcon.src = "/img/icons/icons8-menu.svg";
   } else {
     nav.style.display = "block";
     main.style.display = "none";
+    footer.style.display = "none";
     toggleIcon.src = "/img/icons/icons8-menu_close.svg";
   }
 });
